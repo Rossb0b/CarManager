2ddd3b900803d9ded7c44fffb046d3fd5d0051e6
Pierre Demailly <pierredemailly.pro@gmail.com>
2018-11-27 15:01:48 +0100

    Add bootstrap

--- views/template/footer.php
+++ views/template/footer.php
@@ -6,6 +6,8 @@
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
   <script src="js/plugins.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
   <script src="js/main.js"></script>
 
   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
@@ -17,4 +19,3 @@
 </body>
 
 </html>
-
