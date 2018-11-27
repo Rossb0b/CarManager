66a9c17df4197a2e616cad627ea9e7181d2a774b
Pierre Demailly <pierredemailly.pro@gmail.com>
2018-11-27 16:23:53 +0100

    Fix css & js paths

--- views/template/footer.php
+++ views/template/footer.php
@@ -1,20 +1,17 @@
-<footer>
-  <p>A footer if you need one</p>
-</footer>
 
- <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="./assets/js/vendor/modernizr-2.8.3.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-  <script src="js/plugins.js"></script>
+  <script src="./assets/js/plugins.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-  <script src="js/main.js"></script>
+  <script src="./assets/js/main.js"></script>
 
   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-  <script>
+  <!-- <script>
     window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
     ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
-  </script>
+  </script> -->
   <script src="https://www.google-analytics.com/analytics.js" async defer></script>
 </body>
 
