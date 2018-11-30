1343327825c67f66086be5d0dbe5960d75a4db8c
Pierre Demailly <pierredemailly.pro@gmail.com>
2018-11-30 14:15:41 +0100

    Remove PDO attributs from options

--- conf/database.conf.php
+++ conf/database.conf.php
@@ -1,16 +1,17 @@
 <?php 
+/**
+ * MySQL auth info
+ */
 $host = 'localhost';
 $base = 'week19';
 $user = 'root';
 $pass = 'root';
-$options = [ 
-  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC            
-];
 
 try
 {
-  $database = new PDO("mysql:host=$host;dbname=$base", $user, $pass, $options);
+  $database = new PDO("mysql:host=$host;dbname=$base", $user, $pass);
+  $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $database->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 }
 catch(PDOException $e)
 {
