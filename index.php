a657b2222e9f6aad975dba17a83de705d93d5d08
Pierre Demailly <pierredemailly.pro@gmail.com>
2018-11-30 09:23:53 +0100

    Add error if classes autoloading don't find file

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ spl_autoload_register(function($class)
     require_once './model/' . $class . '.php';
   elseif (file_exists('./entities/' . $class . '.php'))
     require_once './entities/' . $class . '.php';
+  else die('Class ' . $class . ' not found.');
 });
 
 $router = new Router($_GET['page'] ?? null);
